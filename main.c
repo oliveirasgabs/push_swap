f9a353b6c59fd449c73760459fa92dca61090e66
Gabrielly <oliveiragabrielly.dev@gmail.com>
2026-01-15 16:11:47 -0300

    Planejando os passos iniciais

---
+++ main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabrioli <gabrioli@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/15 16:06:07 by gabrioli          #+#    #+#             */
+/*   Updated: 2026/01/15 16:11:24 by gabrioli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	main(int argc, char *argv[])
+{
+	//Verificar se o programa tem menos de 1 argumento ou se são dois argumentos e o segundo argumento é nulo
+		// Caso seja, retornar 1;
+	//Verificar se só tem 2 argumentos
+		//chama afunção split para separar o argv[1][0]
+		//Depois de splitar, nós devemos percorrer a matriz
+			//transformar em atol (alnum to long) cada casinha
+			//mallocar um "prato" com aquele número na stack "a";
+	//
+}
