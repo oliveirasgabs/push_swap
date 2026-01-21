6af8524eddfb3e5e1459487457a56e2f7c1d77d1
Gabrielly <oliveiragabrielly.dev@gmail.com>
2026-01-21 01:24:42 -0300

    construção da chamada principal

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gabrioli <gabrioli@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 16:06:07 by gabrioli          #+#    #+#             */
-/*   Updated: 2026/01/20 18:49:44 by gabrioli         ###   ########.fr       */
+/*   Updated: 2026/01/20 22:41:17 by gabrioli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,26 @@
 
 int	main(int argc, char *argv[])
 {
+	t_stack_node *a;
+	t_stack_node *b;
+
+	a = NULL; //Preciso iniciar null pois é boa pratica
+	b = NULL;
 	if (argc < 2 || (argc == 2 && !argv[1])) //Se o programa tem < de 1 argumento ou se são dois argumentos e o segundo argumento é nulo
 		return (1); // Caso seja, retornar 1;
 	else if (argc == 2) //Verificar se só tem 2 argumentos
-	{
 		argv = ft_split(argv[1], ' '); //chama afunção split para separar o argv[1]
-		//Depois de splitar, nós devemos percorrer a matriz
-			//transformar em atol (alnum to long) cada casinha
-			//mallocar um "prato" com aquele número na stack "a";
-	}//
+	//chama a função que vai jogar os números na stack A
+	init_stack_a(&a, argv + 1); // argv + 1 pois eu passo o segundo argumento, pois o primeiro é o nome do programa
+	if (!stack_sorted(a)) //Se não tiver organizado
+	{
+		if (ft_lstsize(a) == 2) //verifica que a lista só tem 2 pratos
+			sa(&a, false); //sort a
+		else if (ft_lstsize(a) == 3) // se tiver 3 pratos
+			sort_three(&a); //ordena em 3
+		else
+			sort_stacks(&a, &b); //se tiver mais, chama a função maneira
+	}
+	free_stack(&a); //tchau tchau lista
+	return (0);
 }
